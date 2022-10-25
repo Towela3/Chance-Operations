ef3f79c9370f8bf345246f3e58a4f5ce99ebcaa5
Towela3 <112038406+Towela3@users.noreply.github.com~>
2022-10-25 12:27:52 -0400

    try commit

--- sketch/app.js
+++ sketch/app.js
@@ -2,6 +2,14 @@ const pics = [
     'url("1.jpg")',
     'url("2.jpg")',
     'url("3.jpg")',
+    'url("4.jpg")',
+    'url("5.jpg")',
+    'url("6.jpg")',
+    'url("7.jpg")',
+    'url("8.jpg")',
+    'url("9.jpg")',
+    'url("10.jpg")',
+    
 ];
 const pic = document.querySelector('section');
 
